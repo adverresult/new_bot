47e8c9dc49f0404f0e628f376d4a2d287b92192a
adverresult <ccleanairtravel@gmail.com>
2024-10-28 17:15:50 +0400

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       script: "./index.js",        // Путь к вашему индексному файлу
       env: {
         NODE_ENV: "production",     // Переменная окружения для режима
-        API_KEY: "sk-idh-P_B2CbgbYP3LVVHvotKJ90x20FVF3_6FAAxNlMT3BlbkFJyXALiJDPbf5D7v50CgqjLVU_4DKnjsBRamwOf1MwUA",      // Ваш API токен
+        API_KEY: "sk-H5dPJAeafk4MSo_LdR0N12Um8iDnu2H21gfbLICa_1T3BlbkFJMsC5MsDNvBRB35-5Wjjcm5rlSlP6piELLemyPlGUwA",      // Ваш API токен
       },
       // Дополнительные опции
       instances: "max",            // Использовать все доступные CPU
